828b06c4073c2c093a85035df7a48bf11eafa38f
pizza_girl <pizza.girl.dat@gmail.com>
2020-07-16 12:21:57 +0900

    変更

--- Assets/Hensu.cs
+++ Assets/Hensu.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Hensu : MonoBehaviour
 {
+    int a = 10;
     // Start is called before the first frame update
-    void Start()
+    public void Proc()
     {
-        int a = 10;
         float fw = 3.14f;
         string str = "こんにちは";
         bool bl = true;
 
-        Debug.Log(a);
+        Debug.Log(a+a);
         Debug.Log(fw);
         Debug.Log(str);
         Debug.Log(bl);
     }
 
+    public void Proc2()
+    {
+        Debug.Log(a);
+    }
+
         // Update is called once per frame
-        void Update()
+       void Update()
     {
         
     }
